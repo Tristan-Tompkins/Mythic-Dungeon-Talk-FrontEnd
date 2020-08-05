c11ef86f12b0f202adc7621f926b3fa14ac7c701
Tristan-Tompkins <tristantompkins@gmail.com>
2020-08-05 16:52:18 -0400

    Authentication files
    Made my sign in, sign up, change password

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,20 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const authEvents = require('./auth/event')
+
 $(() => {
   // your JS code goes here
 })
+
+$('.signup-view, .logged-in, .change-password-view').hide()
+
+// Authorization event listeners
+  $('#signup').on('submit', authEvents.onSignUp)
+  $('#signin').on('submit', authEvents.onSignIn)
+  $('#signup-button').on('click', authEvents.onSignUpSwitch)
+  $('#signin-button').on('click', authEvents.onSignInSwitch)
+  $('.logout-button').on('click', authEvents.onLogout)
+  $('#change-password').on('submit', authEvents.onChangePassword) // Actually changes password
+  $('.change-password-button').on('click', authEvents.onChangePasswordSwitch) // Changes to the change password screen
+  $('#cancel-change-button').on('click', authEvents.onChangePasswordCancel) // Cancels out of change password screen
