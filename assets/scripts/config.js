16760ee75812cad7358a6ac2228dac46f1bbaaea
trist <tristantompkins@gmail.com>
2020-08-11 10:34:34 -0400

    Fixed heroku link

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://whispering-stream-34803.herokuapp.com/',
+  production: 'https://whispering-stream-34803.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
